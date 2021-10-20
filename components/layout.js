7ddef31134241859c2e244d18ad3fa4e496d9020
Jeffrey de Wit <jeffrey@webdevstudios.com>
2021-10-20 16:31:49 -0400

    Add Layout component

---
+++ components/layout.js
@@ -0,0 +1,29 @@
+import styles from './layout.module.css'
+import Head from 'next/head'
+import Link from 'next/link'
+
+export const siteTitle = 'D&D 5e'
+
+export default function Layout({ children, home }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="D&amp;D with the power of dnd5eapi.co"
+        />
+        <meta name="og:title" content={siteTitle} />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+      <main>{children}</main>
+      {!home && (
+        <div className={styles.backToHome}>
+          <Link href="/">
+            <a>← Back to home</a>
+          </Link>
+        </div>
+      )}
+    </div>
+  )
+}
